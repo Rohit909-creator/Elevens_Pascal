34daafe2fa5ce3b72762e4fedce04b57f5413f18
ROHIT FRANCIS <108980398+Rohit909-creator@users.noreply.github.com>
2025-07-26 05:47:49 +0530

    Bruh Carry consolidation that I made

--- PascalsTriangleNewalgo.py
+++ PascalsTriangleNewalgo.py
@@ -26,31 +26,31 @@ def carry_consolidator(digits:list):
             
             carry = result//10
             remainder = result%10
-            print(f"Digit:{digits[i]} Digit:{shifted_digits[i]} Result: {result} Carry:{carry} Remainder:{remainder}")
-            print(f"Before: {pascal_form}")
+            # print(f"Digit:{digits[i]} Digit:{shifted_digits[i]} Result: {result} Carry:{carry} Remainder:{remainder}")
+            # print(f"Before: {pascal_form}")
             # pascal_form[n-i-1] = remainder
             pascal_form = pascal_form+[remainder]
-            print(f"After pascal_form[n-i-1]: {pascal_form}")
+            # print(f"After pascal_form[n-i-1]: {pascal_form}")
             pascal_form = pascal_form+[carry]
-            print(f"After pascal_form[n-i-2]: {pascal_form}")
+            # print(f"After pascal_form[n-i-2]: {pascal_form}")
         else:
             pascal_form = pascal_form+[result]
             
-    print(f"Digits: {digits}")
-    print(f"ShiftedDigits: {shifted_digits}")
-    print(f'Pascal form: {pascal_form}')
+    # print(f"Digits: {digits}")
+    # print(f"ShiftedDigits: {shifted_digits}")
+    # print(f'Pascal form: {pascal_form}')
     
-    print(f"{digits}")
-    print(f"{shifted_digits}")
-    print(f'{pascal_form}')
+    # print(f"{digits}")
+    # print(f"{shifted_digits}")
+    print(f'{pascal_form[::-1]}')
     return pascal_form[::-1]
 if __name__ == "__main__":
     
     # carry_consolidator(digits=[1,3,3,1])
-    carry_consolidator(digits=[1,5,10,10,5,1])
+    # carry_consolidator(digits=[1,5,10,10,5,1])
+    # carry_consolidator(digits=[1,6,15,20,15,6,1])
     # carry_consolidator(digits=[1])
     
-    # print(elevens_pascal(8))
-            
+    print(elevens_pascal(8))
     
     
