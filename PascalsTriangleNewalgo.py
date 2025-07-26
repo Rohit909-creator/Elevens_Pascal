83f2e96c4a4f8c8944b931d74aa7f8d4e8a54311
ROHIT FRANCIS <108980398+Rohit909-creator@users.noreply.github.com>
2025-07-26 08:37:16 +0530

    Update PascalsTriangleNewalgo.py

--- PascalsTriangleNewalgo.py
+++ PascalsTriangleNewalgo.py
@@ -27,25 +27,13 @@ def carry_consolidator(digits:list):
             
             carry = result//10
             remainder = result%10
-            # print(f"Digit:{digits[i]} Digit:{shifted_digits[i]} Result: {result} Carry:{carry} Remainder:{remainder}")
-            # print(f"Before: {pascal_form}")
-            # pascal_form[n-i-1] = remainder
-            # pascal_form = pascal_form+[remainder]
-            # print(f"After pascal_form[n-i-1]: {pascal_form}")
-            # pascal_form = pascal_form+[carry]
             
             pascal_form = pascal_form +[carry*10+remainder]
             # print(f"After pascal_form[n-i-2]: {pascal_form}")
         else:
             pascal_form = pascal_form+[result]
             
-    # print(f"Digits: {digits}")
-    # print(f"ShiftedDigits: {shifted_digits}")
-    # print(f'Pascal form: {pascal_form}')
     
-    # print(f"{digits}")
-    # print(f"{shifted_digits}")
-    # print(f'{pascal_form[::-1]}')
     return pascal_form[::-1]
 if __name__ == "__main__":
     
